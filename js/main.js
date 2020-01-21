1b0b1b5a3d2411492f5e187c1e286ed89ee3b217
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-01-21 15:23:24 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,102 +1,36 @@
 class Bus {
-
-    startEngine() {
-        console.log('Bus Engine started')
+    constructor() {
+        this.bus = {
+            x: 0, y: 0
+        }
     }
-
-    depositLeave() {
-        console.log('Bus left deposit');
+    getXY() {
+        return this.bus
     }
-
-    turnRight() {
-        console.log('Bus Turn right');
+    up(){
+        this.bus.x += 100;
+        this.bus.y += 15;
     }
-
-    turnLeft(){
-        console.log("Bus turn left")
+    top() {
+        this.bus.x += 25;
     }
-
-    goForward(meters) {
-        console.log(`Bus goes forward ${meters} meters`)
-    }
-
-    arriveAtStation(station) {
-        console.log(`<- Bus arrives at ${station} station`);
-        console.log("Bus opens doors")
-    }
-
-    leavesStation() {
-        console.log("Bus closes doors");
-        console.log("-> Bus leaves stations");
-    }
-
-    passengerTransit(loadCount, unloadCount) {
-        console.log(`Bus unloads ${loadCount} passengers`);
-        console.log(`Bus loads ${unloadCount} passengers`);
+    right() {
+        this.bus.x += 36;
+        this.bus.y += 68;
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("startBusBut").addEventListener("click", () => {
-        const bus = new Bus();
-        bus.startEngine();
-        bus.depositLeave();
-        bus.turnRight();
-        bus.goForward(100);
-        bus.arriveAtStation("Police Department");
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(200);
-        bus.arriveAtStation('Guliver');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(50);
-        bus.turnLeft();
-        bus.goForward(150);
-        bus.arriveAtStation('Dacia');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(100);
-        bus.turnLeft();
-        bus.goForward(200);
-        bus.arriveAtStation('MeraxSoft');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(200);
-        bus.arriveAtStation('Slavena');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(25);
-        bus.turnLeft();
-        bus.goForward(200);
-        bus.arriveAtStation('Spital firoviar');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(250);
-        bus.arriveAtStation('Spital');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(500);
-        bus.arriveAtStation('Cemetery');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(300);
-        bus.arriveAtStation('Steluta');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(500);
-        bus.arriveAtStation('Nikolaeva');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(300);
-        bus.arriveAtStation('Uzina Raut');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-        bus.goForward(50);
-        bus.turnLeft();
-        bus.goForward(200);
-        bus.arriveAtStation('Center');
-        bus.passengerTransit(Math.floor(Math.random()*15), Math.floor(Math.random()*15));
-        bus.leavesStation();
-    });
-});
+const bus = new Bus();
+const div = document.getElementById("bus");
+div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
+    setTimeout(() => {
+        bus.up();
+        div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
+    }, 500);
+    setTimeout(() => {
+        bus.right();
+        div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
+    }, 1000);
+
+
+
