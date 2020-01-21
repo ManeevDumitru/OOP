a86bc86756873eb7f3ac17dc2d06646f03bf2d84
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-01-21 16:58:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,96 +1,48 @@
+//TODO list
+// 1. Reflection
+
 class Bus {
-    constructor() {
-        this.bus = {
-            x: 0, y: 0
+    constructor(bus) {
+        this.position = {
+            x: 0,
+            y: 0
         }
+        this.bus = bus
     }
 
-    getXY() {
-        return this.bus
+    setPosition () {
+    this.bus.setAttribute('style', 'transform: translate(' + this.position.x + 'px,' +  this.position.y + 'px)');
     }
+
+    move (direction) {
+        //
+    }
+
 
     up() {
-        this.bus.y -= 100;
+        this.position.y -= 100;
     }
 
     down() {
-        this.bus.y += 100;
+        this.position.y += 100;
     }
 
     right() {
-        this.bus.x += 100;
+        this.position.x += 100;
     }
 
     left() {
-        this.bus.x -= 100;
+        this.position.x -= 100;
     }
 }
 
-const bus = new Bus();
-const div = document.getElementById("bus");
+const bus = new Bus(document.getElementById("bus"));
+// const route = [
+//     bus.move(''),
+//     bus.move(''),
+//     bus.move(''),
+//     bus.move(''),
+//     bus.move(''),
+// ]
 
-const altaFunctie = () => {
-    div.setAttribute('style', 'transform: translate(' + bus.getXY().x + 'px,' + bus.getXY().y + 'px)');
-}
-setTimeout(() => {
-    bus.up();
-    altaFunctie();
-}, 500);
-setTimeout(() => {
-    bus.right();
-    altaFunctie();
-}, 1000);
-setTimeout(() => {
-    bus.right();
-    altaFunctie();
-}, 1500);
-setTimeout(() => {
-    bus.up();
-    altaFunctie();
-}, 2000);
-setTimeout(() => {
-    bus.up();
-    altaFunctie();
-}, 2500);
-setTimeout(() => {
-    bus.left();
-    altaFunctie();
-}, 2500);
-setTimeout(() => {
-    bus.left();
-    altaFunctie();
-}, 3000);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 3500);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 4000);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 4500);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 5000);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 5500);
-setTimeout(() => {
-    bus.down();
-    altaFunctie();
-}, 6000);
-setTimeout(() => {
-    bus.right();
-    altaFunctie();
-}, 6500);
-setTimeout(() => {
-    bus.right();
-    altaFunctie();
-    console.log('you have arrived')
-}, 7000);
 
