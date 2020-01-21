fbae9815771d8317c6c1498332a5d92998d247de
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-01-21 16:07:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,33 +4,93 @@ class Bus {
             x: 0, y: 0
         }
     }
+
     getXY() {
         return this.bus
     }
-    up(){
-        this.bus.x += 100;
-        this.bus.y += 15;
+
+    up() {
+        this.bus.y -= 100;
     }
-    top() {
-        this.bus.x += 25;
+
+    down() {
+        this.bus.y += 100;
     }
+
     right() {
-        this.bus.x += 36;
-        this.bus.y += 68;
+        this.bus.x += 100;
+    }
+
+    left() {
+        this.bus.x -= 100;
     }
 }
 
 const bus = new Bus();
 const div = document.getElementById("bus");
-div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
-    setTimeout(() => {
-        bus.up();
-        div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
-    }, 500);
-    setTimeout(() => {
-        bus.right();
-        div.setAttribute("style", "transform: Translate(" + bus.getXY().x +"px," + bus.getXY().y + 'px)');
-    }, 1000);
 
-
+const altaFunctie = () => {
+    div.setAttribute('style', 'transform: translate(' + bus.getXY().x + 'px,' + bus.getXY().y + 'px)');
+}
+setTimeout(() => {
+    bus.up();
+    altaFunctie();
+}, 500);
+setTimeout(() => {
+    bus.right();
+    altaFunctie();
+}, 1000);
+setTimeout(() => {
+    bus.right();
+    altaFunctie();
+}, 1500);
+setTimeout(() => {
+    bus.up();
+    altaFunctie();
+}, 2000);
+setTimeout(() => {
+    bus.up();
+    altaFunctie();
+}, 2500);
+setTimeout(() => {
+    bus.left();
+    altaFunctie();
+}, 2500);
+setTimeout(() => {
+    bus.left();
+    altaFunctie();
+}, 3000);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 3500);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 4000);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 4500);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 5000);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 5500);
+setTimeout(() => {
+    bus.down();
+    altaFunctie();
+}, 6000);
+setTimeout(() => {
+    bus.right();
+    altaFunctie();
+}, 6500);
+setTimeout(() => {
+    bus.right();
+    altaFunctie();
+    console.log('you have arrived')
+}, 7000);
 
