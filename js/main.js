f96baa755c84ccb320d2593cf2a54c00d6754f8a
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-01-22 15:05:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,47 +2,83 @@
 // 1. Reflection
 
 class Bus {
-    constructor(bus) {
-        this.position = {
-            x: 0,
-            y: 0
-        }
-        this.bus = bus
-    }
+  constructor(bus) {
+    this.position = {
+      x: 0,
+      y: 0
+    };
+    this.bus = bus
+  }
 
-    setPosition () {
-    this.bus.setAttribute('style', 'transform: translate(' + this.position.x + 'px,' +  this.position.y + 'px)');
-    }
+  setPosition() {
+    this.bus.setAttribute('style', 'transform: translate(' + this.position.x + 'px,' + this.position.y + 'px)')
+  }
 
-    move (direction) {
-        //
-    }
+  MoveUp(i) {
+    setTimeout(() => {
+      this.position.y -= 100;
+      this.setPosition();
+    },i * 1000);
+  }
+
+  MoveDown(i) {
+    setTimeout(() => {
+      this.position.y += 100;
+      this.setPosition()
+    },i * 1000)
 
 
-    up() {
-        this.position.y -= 100;
-    }
+  }
 
-    down() {
-        this.position.y += 100;
-    }
+  MoveRight(i) {
+    setTimeout( () => {
+      this.position.x += 100;
+      this.setPosition();
+    },i * 1000)
 
-    right() {
-        this.position.x += 100;
-    }
+  }
 
-    left() {
-        this.position.x -= 100;
-    }
+  MoveLeft(i) {
+    setTimeout( () => {
+      this.position.x -= 100;
+      this.setPosition();
+    },i * 1000)
+
+  }
 }
 
-const bus = new Bus(document.getElementById("bus"));
-// const route = [
-//     bus.move(''),
-//     bus.move(''),
-//     bus.move(''),
-//     bus.move(''),
-//     bus.move(''),
-// ]
+document.getElementById('startBusBut').addEventListener('click',function () {
+  startGame();
+});
+
+const startGame = () => {
+  const bus = new Bus(document.getElementById("bus"));
+  //const route = [
+  bus.MoveLeft(1);
+  bus.MoveUp(2);
+  bus.MoveRight(3);
+  bus.MoveRight(4);
+  bus.MoveUp(5);
+  bus.MoveUp(6);
+  bus.MoveLeft(7);
+  bus.MoveLeft(8);
+  bus.MoveDown(9);
+  bus.MoveDown(10);
+  bus.MoveDown(11);
+  bus.MoveDown(12);
+  bus.MoveDown(13);
+  bus.MoveDown(14);
+  bus.MoveRight(15);
+  bus.MoveRight(16);
+  bus.MoveRight(17)
+//  ];
+};
+
+
+
+
+
+
+
 
 
