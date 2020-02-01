2b2729570d064532210d28a1e8bfb156350e8d0f
Maneev Dumitru <54940217+ManeevDumitru@users.noreply.github.com>
2020-02-01 17:21:24 +0200

    Add files via upload

--- LAB 1 EX 1 +++/js/main.js
+++ LAB 1 EX 1 +++/js/main.js
@@ -10,51 +10,49 @@ class Calculator {
     this.marks = [];
   }
 
-  addMarks(students, mark) {
-    if (this.marks.length < students && mark > 0 && mark < 11) {
+  addMarks(studentsAmount, mark) {
+    if (this.marks.length < studentsAmount && mark > 0 && mark < 11) {
       this.marks.push(Number(mark));
     }
-    console.log(this.marks)
   }
 
-  calculations() {  // 1
-    let performance = 0;
-    let quality = 0;
-    let min = this.marks[0];
-    let max = this.marks[0];
-    let difference;
-    let media = 0;
-    this.marks.forEach(mark => {
-      if (mark > max) { // max
-        max = mark
+  calculations() {
+    if (this.marks.length !== 0) {
+      let min = this.marks[0];
+      let max = this.marks[0];
+      let quality = 0;
+      let performance = 0;
+      let average = 0;
+      for (let i = 1; i < this.marks.length; i++) {
+        if (this.marks[i] > max) {
+          max = this.marks[i];
+        }
+        if (this.marks[i] < min) {
+          min = this.marks[i];
+        }
       }
-      if (mark < min) { // min
-        min = mark
+      let gap = max - min;
+      for (let i = 0; i < this.marks.length; i++) {
+        if (this.marks[i] >= 5) {
+          performance++;
+          if (this.marks[i] >= 9) {
+            quality++;
+          }
+        }
+        average += this.marks[i];
       }
-      if (mark >= 5) {
-        performance++
+      return {
+        marks: this.marks,
+        min,
+        max,
+        gap,
+        quality: Math.round(quality * 100 / this.marks.length),
+        performance: Math.round(performance * 100 / this.marks.length),
+        average: (average / this.marks.length).toFixed(2)
       }
-      if (mark >= 9) {
-        quality++
-      }
-      media += mark;
-    });
-    difference = max - min;
-    console.log(`
-        Minimum ${min},
-        Maximum ${max},
-        Difference ${difference},
-        Quality ${Math.round(quality * 100 / this.marks.length)}%,
-        Performance ${Math.round(performance * 100 / this.marks.length)}%,
-        Arithmetic Mean ${Math.round(media / this.marks.length)}`);
-    return [
-      min,
-      max,
-      difference,
-      Math.round(quality * 100 / this.marks.length),
-      Math.round(performance * 100 / this.marks.length),
-      Math.round(media / this.marks.length)
-    ]
+    } else {
+      document.getElementById('marks').innerHTML = `No marks to work with`;
+    }
   }
 }
 
@@ -62,12 +60,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const calculations = new Calculator();
   document.getElementById('startCalcBut').addEventListener('click', () => {
     calculations.addMarks(document.getElementById('students').value, document.getElementById('mark').value);
-    const result = calculations.calculations();
-    document.getElementById('min').innerHTML = `Smallest mark is ${result[0]}`;
-    document.getElementById('max').innerHTML = `The biggest mark is ${result[1]}`;
-    document.getElementById('difference').innerHTML = `The difference between min and max is ${result[2]}`;
-    document.getElementById('quality').innerHTML = `The overall quality is ${result[3]}`;
-    document.getElementById('performance').innerHTML = `The performance is ${result[4]}`;
-    document.getElementById('arithmetic').innerHTML = `The average is ${result[5]}`;
+    document.getElementById('marks').innerHTML = `The array is ${calculations.calculations().marks}`;
+    document.getElementById('min').innerHTML = `Smallest mark is ${calculations.calculations().min}`;
+    document.getElementById('max').innerHTML = `The biggest mark is ${calculations.calculations().max}`;
+    document.getElementById('difference').innerHTML = `The difference between min and max is ${calculations.calculations().gap}`;
+    document.getElementById('quality').innerHTML = `The overall quality is ${calculations.calculations().quality}%`;
+    document.getElementById('performance').innerHTML = `The performance is ${calculations.calculations().performance}%`;
+    document.getElementById('average').innerHTML = `The average is ${calculations.calculations().average}`;
   })
 });
